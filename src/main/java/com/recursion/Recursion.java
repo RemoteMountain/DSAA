621f8c084e40d9b3c1e619241d57fe76d2128f6b
liwang <m15077901882@163.com>
2020-06-17 18:12:24 +0800

    插入排序

--- src/main/java/com/recursion/Recursion.java
+++ src/main/java/com/recursion/Recursion.java
@@ -11,13 +11,13 @@ import java.util.Map;
 public class Recursion {
 
     //最终推荐人
-    public long findRootReferrerId(long actorId) {
+    /*public long findRootReferrerId(long actorId) {
         Long referrerId = select referrer_id from[ table]where actor_id = actorId;
         if (referrerId == null) {
             return actorId;
         }
         return findRootReferrerId(referrerId);
-    }
+    }*/
 
     int depth = 0;
 

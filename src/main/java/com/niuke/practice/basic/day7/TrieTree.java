14c085f594eab48a9861639882d137d8ce722600
JamesPang <m15077901882@163.com>
2021-05-15 18:59:50 +0800

    day7

--- src/main/java/com/niuke/practice/basic/day7/TrieTree.java
+++ src/main/java/com/niuke/practice/basic/day7/TrieTree.java
@@ -23,71 +23,19 @@ public class TrieTree {
             }
 
             public void insert(String word) {
-                if (word == null) {
-                    return;
-                }
-                char[] chs = word.toCharArray();
-                TrieNode node = root;
-                int index = 0;
-                for (int i = 0; i < chs.length; i++) {
-                    index = chs[i] - 'a';
-                    if (node.nexts[index] == null) {
-                        node.nexts[index] = new TrieNode();
-                    }
-                    node = node.nexts[index];
-                    node.path++;
-                }
-                node.end++;
+
             }
 
             public void delete(String word) {
-                if (search(word) > 0) {
-                    char[] chs = word.toCharArray();
-                    TrieNode node = root;
-                    int index = 0;
-                    for (int i = 0; i < chs.length; i++) {
-                        index = chs[i] - 'a';
-                        if (--node.nexts[index].path == 0) {
-                            node.nexts[index] = null;
-                            return;
-                        }
-                        node = node.nexts[index];
-                    }
-                    node.end--;
-                }
+
             }
 
             public int search(String word) {
-                if (word == null) {
-                    return 0;
-                }
-                char[] chs = word.toCharArray();
-                TrieNode node = root;
-                int index = 0;
-                for (int i = 0; i < chs.length; i++) {
-                    index = chs[i] - 'a';
-                    if (node.nexts[index] == null) {
-                        return 0;
-                    }
-                    node = node.nexts[index];
-                }
-                return node.end;
+                return 0;
             }
 
             public int prefixNumber(String pre) {
-                if (pre == null) {
-                    return 0;
-                }
-                char[] chs = pre.toCharArray();
-                TrieNode node = root;
-                int index = 0;
-                for (int i = 0; i < chs.length; i++) {
-                    if (node.nexts[index] == null) {
-                        return 0;
-                    }
-                    node = node.nexts[index];
-                }
-                return node.path;
+                return 0;
             }
 
             public static void main(String[] args) {

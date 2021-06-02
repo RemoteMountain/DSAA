d3fa3a106a5c8736bf17f69496c0ab0ad0263402
JamesPang <m15077901882@163.com>
2021-06-02 21:56:06 +0800

    day1

--- src/main/java/com/niuke/basic/day8/Knapsack.java
+++ src/main/java/com/niuke/basic/day8/Knapsack.java
@@ -3,24 +3,33 @@ package com.niuke.basic.day8;
 public class Knapsack {
 
     public static int process1(int[] weights, int[] values, int i, int w, int v) {
-        if (i == weights.length || w-weights[i]<0){
+        if (i == weights.length || w - weights[i] < 0) {
             return v;
         }
-        int v1 = process1(weights,values,i+1,w,v);
-        int v2 = process1(weights,values,i+1,w-weights[i],v+values[i]);
-        return Math.max(v1,v2);
+        int v1 = process1(weights, values, i + 1, w, v);
+        int v2 = process1(weights, values, i + 1, w - weights[i], v + values[i]);
+        return Math.max(v1, v2);
     }
 
-    public static int process2(int[] weights, int[] values,int w){
-        return 0;
+    public static int process2(int[] weights, int[] values, int w) {
+        int[][] dp = new int[weights.length + 1][w + 1];
+        for (int i = weights.length - 1; i >= 0; i--) {
+            for (int j = w; j >= 0; j--) {
+                dp[i][j] = dp[i + 1][j];
+                if (j + weights[i] <= w) {
+                    dp[i][j] = Math.max(dp[i + 1][j], values[i] + dp[i + 1][j + weights[i]]);
+                }
+            }
+        }
+        return dp[0][0];
 
     }
-
 
 
     public static void main(String[] args) {
-        int[] weights = {1,1,1,1,5};
-        int[] values = {1,1,1,1,5};
-        System.out.println(process1(weights,values,0,6,0));
+        int[] weights = { 3, 2, 4, 7 };
+        int[] values = { 5, 6, 3, 19 };
+        System.out.println(process1(weights, values, 0, 6, 0));
+        System.out.println(process2(weights, values, 6));
     }
 }

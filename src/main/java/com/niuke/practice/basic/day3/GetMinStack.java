5759bb58d667b4dc5bb927d1506e2da091f6dfd8
JamesPang <m15077901882@163.com>
2021-06-15 21:10:36 +0800

    GetMinStack

--- src/main/java/com/niuke/practice/basic/day3/GetMinStack.java
+++ src/main/java/com/niuke/practice/basic/day3/GetMinStack.java
@@ -4,24 +4,72 @@ package com.niuke.practice.basic.day3;
 import java.util.Stack;
 
 public class GetMinStack {
+    public static class MyStack1 {
+        private Stack<Integer> stackData;
+        private Stack<Integer> stackMin;
 
-    private Stack<Integer> data;
-    private Stack<Integer> min;
+        public MyStack1() {
+            this.stackData = new Stack<Integer>();
+            this.stackMin = new Stack<Integer>();
+        }
 
-    public GetMinStack() {
-        data = new Stack();
-        min = new Stack();
+        public void push(int newNum) {
+
+        }
+
+        public int pop() {
+            return 0;
+        }
+
+
+        public int getMin() {
+            return 0;
+        }
     }
 
-    public void push(int value) {
+    public static class MyStack2 {
+        private Stack<Integer> stackData;
+        private Stack<Integer> stackMin;
 
+        public MyStack2() {
+            this.stackData = new Stack<Integer>();
+            this.stackMin = new Stack<Integer>();
+        }
+
+        public void push(int newNum) {
+
+        }
+
+        public Integer pop() {
+            return 0;
+        }
+
+        public Integer getMin() {
+            return 0;
+        }
     }
 
-    public Integer pop() {
-        return null;
-    }
+    public static void main(String[] args) {
+        MyStack1 stack1 = new MyStack1();
+        stack1.push(3);
+        System.out.println(stack1.getMin());
+        stack1.push(4);
+        System.out.println(stack1.getMin());
+        stack1.push(1);
+        System.out.println(stack1.getMin());
+        System.out.println(stack1.pop());
+        System.out.println(stack1.getMin());
 
-    public Integer getMin() {
-        return null;
+        System.out.println("=============");
+
+        MyStack1 stack2 = new MyStack1();
+        stack2.push(3);
+        System.out.println(stack2.getMin());
+        stack2.push(4);
+        System.out.println(stack2.getMin());
+        stack2.push(1);
+        System.out.println(stack2.getMin());
+        System.out.println(stack2.pop());
+        System.out.println(stack2.getMin());
     }
 }

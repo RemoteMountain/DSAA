a0bcead4403f1f3e7cc15e91481a9ebd88faec92
JamesPang <m15077901882@163.com>
2021-06-27 17:46:55 +0800

    MaximalRectangle

---
+++ src/main/java/com/niuke/advance/day2/MaximalRectangle.java
@@ -0,0 +1,49 @@
+package com.niuke.advance.day2;
+
+import java.util.Stack;
+
+//最大子矩阵
+public class MaximalRectangle {
+
+    public static int maxRecSize(int[][] map) {
+        if (map == null || map.length == 0 || map[0].length == 0) {
+            return 0;
+        }
+        int maxArea = 0;
+        int[] height = new int[map[0].length];
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[0].length; j++) {
+                height[j] = map[i][j] == 0 ? 0 : height[j] + 1;
+            }
+            maxArea = Math.max(maxRecFromBottom(height), maxArea);
+
+        }
+        return maxArea;
+    }
+
+    //[4,3,2,5,1]
+    public static int maxRecFromBottom(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+        int maxArea = 0;
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < height.length; i++) {
+            while (!stack.isEmpty() && height[i] <= stack.peek()) {
+                int j = stack.pop();
+                //k：左边界 ，i：右边界
+                int k = stack.isEmpty() ? -1 : stack.peek();
+                int curArea = (i - k - 1) * height[j];
+                maxArea = Math.max(maxArea, curArea);
+            }
+            stack.push(i);
+        }
+        while (!stack.isEmpty()) {
+            int j = stack.pop();
+            int k = stack.isEmpty() ? -1 : stack.peek();
+            int curArea = (height.length - k - 1) * height[j];
+            maxArea = Math.max(maxArea, curArea);
+        }
+        return maxArea;
+    }
+}

f23854ca2ca2dfe736cc48e8dcd4bd5a78cb0e25
JamesPang <m15077901882@163.com>
2021-08-22 14:29:13 +0800

    leetcode76-最小覆盖子串

--- src/main/java/com/leetcode/Leetcode76_MinConverSubString.java
+++ src/main/java/com/leetcode/Leetcode76_MinConverSubString.java
@@ -1,33 +1,49 @@
 package com.leetcode;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class Leetcode76_MinConverSubString {
 
-    public String minWindow(String s, String t) {
-        if (s == null || t == null) {
+    public static String minWindow(String s, String t) {
+        if (s == null || s.length() == 0 || t == null || t.length() == 0) {
             return "";
         }
-        char[] sArr = s.toCharArray();
-        char[] tArr = t.toCharArray();
-        Set<Character> tSet = new HashSet<>();
-        for (int i = 0; i < tArr.length; i++) {
-            tSet.add(tArr[i]);
+        int[] need = new int[128];
+        for (int i = 0; i < t.length(); i++) {
+            need[t.charAt(i)]++;
         }
-        StringBuilder subBuf = new StringBuilder("");
-        String sub = "";
-        for (int i = 0; i < sArr.length; i++) {
-            for (int j = i; j < sArr.length; j++) {
-                if (!tSet.contains(sArr[j])) {
-                    break;
-                }
-                if ()
-                    subBuf.append(sArr[j]);
+        int i = 0;
+        int j = 0;
+        int start = 0;
+        int needCnt = t.length();
+        int minSize = Integer.MAX_VALUE;
+        while (j < s.length()) {
+            if (need[s.charAt(j)] > 0) {
+                needCnt--;
             }
+            need[s.charAt(j)]--;
+            if (needCnt == 0) {
+                while (i < j && need[s.charAt(i)] < 0) {
+                    need[s.charAt(i)]++;
+                    i++;
+                }
+                if (j - i + 1 < minSize) {
+                    minSize = j - i + 1;
+                    start = i;
+                }
+                need[s.charAt(i)]++;
+                i++;
+                needCnt++;
 
-
+            }
+            j++;
         }
+        return minSize == Integer.MAX_VALUE ? "" : s.substring(start, start + minSize);
+    }
 
+
+    public static void main(String[] args) {
+        String s = "ADOBECODEBANC";
+        String t = "ABC";
+        System.out.println(minWindow(s, t));
+        System.out.println(s.substring(0, 2));
     }
 }

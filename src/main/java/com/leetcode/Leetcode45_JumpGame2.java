df5a783bf3434e899594e41d9921f2dca9b2ff9a
JamesPang <m15077901882@163.com>
2021-08-22 15:20:46 +0800

    leetcode45-跳跃游戏2

--- src/main/java/com/leetcode/Leetcode45_JumpGame2.java
+++ src/main/java/com/leetcode/Leetcode45_JumpGame2.java
@@ -3,17 +3,18 @@ package com.leetcode;
 public class Leetcode45_JumpGame2 {
 
     public int jump(int[] nums) {
-        if (nums == nums || nums.length < 2) {
+        if (nums == null || nums.length < 2) {
             return 0;
         }
         int res = 0;
         int rightmost = 0;
         int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            res++;
-            rightmost = Math.max(rightmost, i + nums[i]);
-            if (rightmost >= n - 1) {
-                break;
+        int end = 0;
+        for (int i = 0; i < n - 1; i++) {
+            rightmost = Math.max(rightmost,i+nums[i]);
+            if (i == end){
+                end = rightmost;
+                res++;
             }
         }
         return res;

cc64b93047cc1ff3c250c7e6755469cb68a3cd79
JamesPang <m15077901882@163.com>
2021-10-10 19:48:11 +0800

    day6

--- src/main/java/com/niuke/advance/day6/CardsInLine.java
+++ src/main/java/com/niuke/advance/day6/CardsInLine.java
@@ -22,4 +22,27 @@ public class CardsInLine {
         }
         return Math.min(f(arr, i + 1, j), f(arr, i, j - 1));
     }
+
+    public static int win2(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return 0;
+        }
+        int[][] f = new int[arr.length][arr.length];
+        int[][] s = new int[arr.length][arr.length];
+        for (int j = 0; j < arr.length; j++) {
+            f[j][j] = arr[j];
+            for (int i = j - 1; i >= 0; i--) {
+                f[i][j] = Math.max(arr[i] + s[i + 1][j], arr[j] + s[i][j - 1]);
+                s[i][j] = Math.min(f[i + 1][j], f[i][j - 1]);
+            }
+        }
+        return Math.max(f[0][arr.length - 1], s[0][arr.length - 1]);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 9, 1 };
+        System.out.println(win1(arr));
+        System.out.println(win2(arr));
+
+    }
 }
